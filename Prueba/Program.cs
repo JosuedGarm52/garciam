2da9a6263340136b3bb15fb5cdcafce93b574450
JonnyJaccob <jaccobjosue@gmail.com>
2020-09-02 00:11:46 -0500

    Inicializar proyecto

---
+++ Prueba/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace git_man
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
